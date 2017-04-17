4feb102ca42d664d9a9eaf2295e0368099a90c0d
Kinrany <kinrany@yandex.com>
2017-04-17 19:13:36 +0300

    removed folders sketch2, sketch3, sketch4

--- sketch4/drawable.lua
+++
@@ -1,9 +0,0 @@
-local Drawable = {}
-
-function Drawable.draw(self)
-	local box, color = self.box, self.color
-	love.graphics.setColor(color:unpack())
-	love.graphics.rectangle("fill", box:unpack())
-end
-
-return Drawable
