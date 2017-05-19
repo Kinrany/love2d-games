41499c17c276ead0e2b05b566a893d41f02a2352
Kinrany <kinrany@yandex.com>
2017-05-19 17:44:04 +0300

    added conf.lua with common libraries

---
+++ project/conf.lua
@@ -0,0 +1,6 @@
+_G.Inspect = require "lib/inspect"
+_G.Memoize = require "lib/memoize"
+_G.Class   = require "lib/middleclass"
+_G.__      = require "lib/underscore"
+
+_G.Lovebird = require "lib/lovebird/lovebird"
