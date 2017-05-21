db88deea3d6525a8e35513afcfa46e7119956d3d
Kinrany <kinrany@yandex.com>
2017-05-21 22:41:09 +0300

    image manager now handles errors correctly

--- project/ImageManager.lua
+++ project/ImageManager.lua
@@ -2,19 +2,31 @@ local ImageManager = {}
 
 local images = {}
 
+-- provides error message as second return value
 function ImageManager.get(name)
 	local image = images[name]
 	if image then
 		return image
 	end
 	
-	image = ImageManager.load(name)
+	image, error_message = ImageManager._tryLoad(name)
 	images[name] = image
-	return image
+	return image, error_message
 end
 
-function ImageManager.load(name)
+function ImageManager._load(name)
 	return love.graphics.newImage("resources/" .. name .. ".png")
+end
+
+-- on success returns image
+-- on failure returns nil and error message
+function ImageManager._tryLoad(name) 
+	local success, image_or_error = pcall(love.graphics.newImage, "resources/" .. name .. ".png")
+	if success then
+		return image_or_error -- image
+	else
+		return nil, image_or_error -- nil, error
+	end
 end
 
 return ImageManager
