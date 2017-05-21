95e14d6c135980154454e8524d6ef984875db527
Kinrany <kinrany@yandex.com>
2017-05-22 00:51:20 +0300

    PositionComponent can be moved in given direction

--- project/components/Position.lua
+++ project/components/Position.lua
@@ -1,8 +1,26 @@
+local Direction = require "classes/Direction"
+
+
 local PositionComponent = Class('PositionComponent')
 
 function PositionComponent.initialize(self, x, y)
 	self.x = assert(x)
 	self.y = assert(y)
+end
+
+-- move position in given absolute direction
+function PositionComponent.move(self, direction)
+	if direction == Direction.RIGHT then
+		self.x = self.x + 1
+	elseif direction == Direction.LEFT then
+		self.x = self.x - 1
+	elseif direction == Direction.FRONT then
+		self.y = self.y - 1
+	elseif direction == Direction.BACK then
+		self.y = self.y + 1
+	else
+		error("Unknown direction: " .. tostring(direction))
+	end
 end
 
 return PositionComponent
