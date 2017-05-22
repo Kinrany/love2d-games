bdc9f8dc7eff71b5468b853419d4d5fcfcc860bb
Kinrany <kinrany@yandex.com>
2017-05-22 23:35:40 +0300

    Tile is a separate class that contains a sprite

--- project/Tile.lua
+++ project/Tile.lua
@@ -1,5 +1,11 @@
-return class("Tile", require "Sprite")
+local Sprite = require "Sprite"
 
---[[
 
---]]
+
+local Tile = class("Tile")
+
+function Tile.initialize(self, name, x, y)
+	self.sprite = Sprite:new(name, x, y)
+end
+
+return Tile
