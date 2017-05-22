9d6de7072a494137b9c7247dd3476eacc7345f15
Kinrany <kinrany@yandex.com>
2017-05-22 23:39:15 +0300

    Player can move around

--- project/main.lua
+++ project/main.lua
@@ -1,25 +1,63 @@
 local Dir = require "Direction"
 local Sprite = require "Sprite"
+local Tile = require "Tile"
 
-local sprites = {}
-local function add_sprite(name, x, y)
-	local sprite = Sprite:new(name, x, y)
-	table.insert(sprites, sprite)
-	return sprite
+
+
+local entities = {}
+local function add_entity(entity)
+	table.insert(entities, entity)
+	return entity
 end
 
-add_sprite("grass", 0, 0)
-add_sprite("brick", 5, 10)
-add_sprite("human", 8, 6)
+for x = 0, 20 do
+	for y = 0, 15 do
+		local tile_name;
+		if love.math.random(1, 5) <= 2 then
+			tile_name = "stone"
+		else
+			tile_name = "grass"
+		end
+		add_entity(Tile:new(tile_name, x, y))
+	end
+end
 
+local brick_sprite = add_entity(Sprite:new("brick", 5, 10))
+local human_sprite = add_entity(Sprite:new("human", 8,  6))
+
+function love.keypressed(key, scancode, isrepeat)
+	if key == "left" then
+		human_sprite:turn(Dir.left)
+	end
+	if key == "right" then
+		human_sprite:turn(Dir.right)
+	end
+	if key == "up" then
+		human_sprite:moveRelative(Dir.front)
+	end
+	if key == "down" then
+		human_sprite:turn(Dir.back)
+	end
+end
 
 local function draw_all_sprites()
 	local TILE_WIDTH = 32
 	for sprite, x, y in Sprite:iterAll() do
-		love.graphics.draw(sprite:getImage(), x * TILE_WIDTH, y * TILE_WIDTH)
+		local image = sprite:getImage()
+		local screen_x = x * TILE_WIDTH
+		local screen_y = y * TILE_WIDTH
+		love.graphics.draw(image, screen_x, screen_y)
+		
+		-- hack for drawing player above other sprites
+		local hX, hY = human_sprite:getPos()
+		if x == hX and y == hY then
+			local image = human_sprite:getImage()
+			love.graphics.draw(image, screen_x, screen_y)
+		end
 	end
 end
 
 function love.draw()
+	love.graphics.setBackgroundColor(64, 128, 64)
 	draw_all_sprites()
 end
